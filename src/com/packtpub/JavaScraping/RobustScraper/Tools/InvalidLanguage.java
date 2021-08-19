7bec1249f424f8f2a21db33fa958e7715723c347
Dan Whyte <danwhyte1986@gmail.com>
2021-08-20 00:05:07 +0100

    Added in Invalid Language, IPResolver, PageDoesNotExist and Webpage

--- src/com/packtpub/JavaScraping/RobustScraper/Tools/InvalidLanguage.java
+++ src/com/packtpub/JavaScraping/RobustScraper/Tools/InvalidLanguage.java
@@ -1,4 +1,13 @@
+/* InvalidLanguage.java is the exception method that is thrown when the language given to the Wikipedia class does not exist: */
+
 package com.packtpub.JavaScraping.RobustScraper.Tools;
 
-public class InvalidLanguage {
+/** * Is thrown if a two letter language code (e.g. "en," "fr," "de") does not actually exist */
+
+public class InvalidLanguage extends Exception {
+
+    private static final long serialVersionUID = 1L;
+    public InvalidLanguage(String msg) { System.out.println(msg);
+    }
 }
+
--- src/com/packtpub/JavaScraping/RobustScraper/Tools/PageDoesNotExist.java
+++ src/com/packtpub/JavaScraping/RobustScraper/Tools/PageDoesNotExist.java
@@ -1,4 +1,10 @@
 package com.packtpub.JavaScraping.RobustScraper.Tools;
 
-public class PageDoesNotExist {
+/** * Error is thrown if the webpage does not exist */
+
+public class PageDoesNotExist extends Exception {
+
+    public PageDoesNotExist(String msg) { System.out.println(msg);
+    }
 }
+
