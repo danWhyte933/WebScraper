eea9dc27358954a5b19a621cd9b67e5831a090fd
Dan Whyte <danwhyte1986@gmail.com>
2021-08-21 16:45:25 +0100

    Create the WikiSQL file

---
+++ src/com/packtpub/JavaScraping/PersistingData/WikiSQL.java
@@ -0,0 +1,62 @@
+package com.packtpub.JavaScraping.PersistingData;
+
+import com.packtpub.JavaScraping.HandlingErrors.LinkNotFoundException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Random;
+import java.sql.*;
+
+public class WikiSQL { private static Random generator; private static Connection dbConn;
+// need mySQL username password and database
+    public static void main(String[] args) {
+        generator = new Random(31415926);
+        for (int i = 0; i < 10; i++) {
+            try {
+
+
+                scrapeTopic("/wiki/Java");
+            } catch (LinkNotFoundException e) {
+                System.out.println("Trying again!");
+            }
+        }
+    }
+
+    public static void scrapeTopic(String url) throws LinkNotFoundException {
+        String html = getUrl("https://en.wikipedia.org/"+url);
+        Document doc = Jsoup.parse(html);
+
+        Elements links = doc.select("#mw-content-text [href~=^/wiki/((?!:).)*$]");
+
+        if(links.size() == 0){
+            throw new LinkNotFoundException("No links no page, or page malformed");
+        }
+        int r = generator.nextInt(links.size());
+        System.out.println("Random link is: "+links.get(r).text()+" at url: "+links.get(r).attr("href"));
+
+        scrapeTopic(links.get(r).attr("href"));
+    }
+
+    public static String getUrl(String url){
+        URL urlObj = null; try{ urlObj = new URL(url);
+        }
+        catch(MalformedURLException e){
+            System.out.println("The url was malformed!"); return "";
+        }
+        URLConnection urlCon = null; BufferedReader in = null; String outputText = ""; try{
+            urlCon = urlObj.openConnection();
+            in = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+            String line = "";
+            while((line = in.readLine()) != null){ outputText += line; } in.close(); }catch(IOException e){
+            System.out.println("There was an error connecting to the URL"); return ""; } return outputText;
+    }
+
+}
+
